f5dd76978cc82de8ecd6ac8ac0944ae6fccad67b
NasKar2 <jschwrtz9@gmail.com>
2019-08-25 11:47:12 -0400

    added lazylibrarian to allbackup.sh

--- allbackup.sh
+++ allbackup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-# run all 5 backup programs, set cron="yes" for each config to automate backup
+# run all 7 backup programs, set cron="yes" for each config to automate backup
 /git/backupapps-freenas-iocage/sonarrbackup.sh
 /git/backupapps-freenas-iocage/radarrbackup.sh
 /git/backupapps-freenas-iocage/lidarrbackup.sh
 /git/backupapps-freenas-iocage/sabnzbdbackup.sh
 /git/backupapps-freenas-iocage/tautullibackup.sh
 /git/backupapps-freenas-iocage/wpBackup.sh
+/git/backupapps-freenas-iocage/lazylibrarianbackup.sh
 echo "Done"
